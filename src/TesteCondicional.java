cb392bccc41484687bd0e4d23b866e0b8ec7da57
pauloeufrasio <henriquepaulo0606@gmail.com>
2023-02-20 16:31:07 -0300

    Initial commit

---
+++ src/TesteCondicional.java
@@ -0,0 +1,13 @@
+public class TesteCondicional {
+
+    public static void main(String[] args) {
+
+        System.out.println("Testando condicionais ");
+        int idade = 20;
+        if ( idade >= 18) {
+            System.out.println("Voce tem mais de 18 anos "); // Testando condição
+        }
+
+    }
+
+}
