a4df7b32b317f2d2a0c0925df6f337c2f6bdec17
paulo <henriquepaulo0606@gmail.com>
2023-03-23 15:03:40 -0300

    Testando condicionais e verificando se é maior que 18

--- src/TesteCondicional.java
+++ src/TesteCondicional.java
@@ -1,18 +1,26 @@
+import java.util.Scanner;
+
 public class TesteCondicional {
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
         System.out.println("Testando condicionais ");
-        int idade = 20;
-        int quantidadePessoa = 3;
-        if ( idade >= 18) {
+        // Atribuindo valores para variaveis
+        System.out.println("Digite sua idade: ");
+        int idade = sc.nextInt();
+        System.out.println("Quantas pessoas voce está acompanhado: ");
+        int quantidadePessoas = sc.nextInt();
+        // Testando se a condição idade e maior ou igual 18
+        if (idade >= 18) {
             System.out.println("Voce tem mais de 18 anos "); // Testando condição
+            System.out.println("Seja bem vindo");
         }
-        else if (quantidadePessoa >= 2){
+        // Se caso a idade for menor que 18 vamos testar uma nova condição se a quantidadePessoa for maior que 2 e for maiores de idade
+        else if (quantidadePessoas >= 2) { // testando condiçao
             System.out.println("Voce não tem 18, mais pode entrar, pois está acompanhado por maiores ");
-        }
-        else {
-            System.out.println("Infelizmente voce não pode entrar");
+        } else {
+            System.out.println("Infelizmente voce não pode entrar, pois precisa de um adulto!! ");
         }
     }
 
